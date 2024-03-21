53cfb22c95f113191c4656d401640483f616c37a
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-03-21 23:59:04 +0100

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    }
 
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
+
+    if (a > b) {
+        let c = a;
+        a = b;
+        b = c;
+    }
+    let sum = a;
+
+    for (i = a + 1; i <= b; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
