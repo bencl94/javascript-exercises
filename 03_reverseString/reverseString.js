3955b31910067c88dfcc9bfa6b770b43779e1168
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-03-21 23:29:46 +0100

    Optimize 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,5 @@
 const reverseString = function (str) {
-    let result = [];
-    let strArray = str.split('');
-
-    for (let i = strArray.length - 1; i >= 0; i--) {
-        result[strArray.length - 1 - i] = str[i];
-    }
-
-    return result.join('');
+    return str.split('').reverse().join('');
 };
 
 // Do not edit below this line
