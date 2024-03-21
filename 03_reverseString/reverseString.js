175600a6fdd1b78ddd25ed07691eb4cfc0e1651b
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-03-21 23:28:10 +0100

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    let result = [];
+    let strArray = str.split('');
 
+    for (let i = strArray.length - 1; i >= 0; i--) {
+        result[strArray.length - 1 - i] = str[i];
+    }
+
+    return result.join('');
 };
 
 // Do not edit below this line
