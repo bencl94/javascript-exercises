ea4013eea4350dbc02b286204457b96d46ac391d
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-03-29 00:31:46 +0100

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const fibonacci = function (f) {
+    if (f < 0) { return 'OOPS' };
+    if (f == 0) { return 0 };
+    if (f == 1) { return 1 };
 
+    return fibonacci(f - 1) + fibonacci(f - 2);
 };
 
 // Do not edit below this line
