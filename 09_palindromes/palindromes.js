f7df517818d76b8e02309ac3b6f98994c78c21f9
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-03-29 00:15:55 +0100

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let whiteSpace = /\s/g;
+    str = str.replace(whiteSpace, '');
 
+    let punctuation = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+    str = str.replace(punctuation, '');
+
+    for (let i = 0, j = str.length - 1; i < str.length; i++, j--) {
+        if (str[i].toLowerCase() != str[j].toLowerCase()) { return false; }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
