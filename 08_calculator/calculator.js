e5fe37dc5f4a53f4912341f906e5f3698f45c314
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-03-29 00:00:52 +0100

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((prev, curr,) => prev + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prev, curr) => prev * curr);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (f) {
+  const memory = [];
+  memory[0] = 1;
+
+  for (let i = 1; i <= f; i++) {
+    memory[i] = i * memory[i - 1];
+  }
+
+  return memory[f];
 };
 
 // Do not edit below this line
