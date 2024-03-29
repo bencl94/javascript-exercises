21dc54fb83cfcf9d903afa83841a3cb7ad620023
Benjamin Clauß <90100610+bencl94@users.noreply.github.com>
2024-03-29 22:38:30 +0100

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,32 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = {
+        name: '', age: 0
+    };
 
+    people.forEach((person) => {
+        if (oldest.name === '') {
+            oldest = { ...person };
+            oldest.age = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        }
+
+        person.age = getAge(person.yearOfBirth, person.yearOfDeath);
+
+        if (person.age > oldest.age) {
+            oldest = { ...person };
+        }
+    });
+
+    return oldest;
 };
+
+const getAge = function (birth, death) {
+
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
